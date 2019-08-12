b9508a35ca5ecfb82b0348793035f51b714035e7
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2019-08-12 20:58:58 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import useJsonFetch from './hooks/useJsonFetch';
+
+function AppHook ({ url }) {
+  const[data, isLoading, hasError] = useJsonFetch(url)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data && data.status}</h1>
+      <h1>{isLoading && 'Is Loading'}</h1>
+      <h1>{hasError && 'Has Error'}</h1>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return (<div>
+    <AppHook url={process.env.REACT_APP_DATA_URL} />
+    <hr />
+    <AppHook url={process.env.REACT_APP_ERROR_URL} />
+    <hr />
+    <AppHook url={process.env.REACT_APP_LOADING_URL} />
+    <hr />
+  </div>)
+}
